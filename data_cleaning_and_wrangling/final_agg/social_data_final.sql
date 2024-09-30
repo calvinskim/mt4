2ea5b98e24737abe09d1c627ddbb3d2c764d176c
Kim, Calvin <ckim369@gatech.edu>
2024-09-29 20:42:17 -0400

    Create social_data_final.sql

---
+++ data_cleaning_and_wrangling/final_agg/social_data_final.sql
@@ -0,0 +1,68 @@
+create or replace table `marketeam_data.social_data_final`
+(sustainer int64,
+gender int64,
+client_state_rank int64,
+all_state_rank int64,
+client_zip_rank int64,
+all_zip_rank int64,
+age int64,
+account_id string,
+gift_date date,
+gift_min_t12m decimal,
+gift_max_t12m decimal,
+gift_avg_t12m decimal,
+gift_count_t12m int64,
+promo_count_t12m int64,
+cons_month_t12m int64,
+dm_t12m int64,
+tm_t12m int64,
+wm_t12m int64,
+digital_t12m int64,
+f2f_t12m int64,
+events_t12m int64,
+drtv_t12m int64,
+other_t12m int64,
+no_channel_t12m int64,
+member_t12m int64,
+gift_min_lt decimal,
+gift_max_lt decimal,
+gift_avg_lt decimal,
+gift_count_lt int64,
+promo_count_lt int64,
+cons_month_lt int64
+)
+as
+((select
+  case when c.sustainer_flag then 1 else 0 end as sustainer,
+  c.* except(sustainer_flag,account_id,city,state,zip),
+  d.* except(first_sustainer,sustainer_flag)
+from
+  `marketeam_data.social_data_temp` d
+left join
+  `marketeam_data.social_samples` s
+  on d.account_id = s.account_id
+left join
+  `marketeam_data.social_donor_clean` c
+  on d.account_id = c.account_id
+where
+  d.first_sustainer is false
+  and d.sustainer_flag is false
+  and s.sustainer_flag is false
+  )
+union all
+(select
+  case when c.sustainer_flag then 1 else 0 end as sustainer,
+  c.* except(sustainer_flag,account_id,city,state,zip),
+  d.* except(first_sustainer,sustainer_flag)
+from
+  `marketeam_data.social_data_temp` d
+left join
+  `marketeam_data.social_samples` s
+  on d.account_id = s.account_id
+left join
+  `marketeam_data.social_donor_clean` c
+  on d.account_id = c.account_id
+where
+  d.first_sustainer is true
+  and d.sustainer_flag is true
+  and s.sustainer_flag is true))
